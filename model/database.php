25fb8f62f4499cb1cb4db5c9ba606265b1a16c5c
DavidYandun <davidcad963@gmail.com>
2018-05-10 08:22:51 -0500

    CambioConexionBDD_Azure_1

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=bdd;charset=utf8', 'root', '');
+        $pdo = new PDO('mysql:host=yandunbddserver.mysql.database.azure.com;dbname=bdd;charset=utf8', 'david@yandunbddserver', 'ch0c0latE');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
